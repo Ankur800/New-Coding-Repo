f461b038ff1cd3a08be48efe95bdde59804a4a11
Ankur800 <ankurrai800@gmail.com>
2021-05-05 17:24:14 +0530

    Frequently asked question

--- CPP/Stack/nextGreaterElement.cpp
+++ CPP/Stack/nextGreaterElement.cpp
@@ -8,8 +8,31 @@
 #include<stack>
 using namespace std;
 
+void nextGreater(vector<int> &arr) {
+	if(arr.size() == 0) {
+		return;
+	}
+	int n = arr.size();
+	vector<int> res(n);
+	stack<int> st;
+	st.push(arr[n-1]);
+	res[n-1] = -1;
+	for(int i=n-2;i>=0;i--) {
+		while(!st.empty() && arr[i] > st.top()) {
+			st.pop();
+		}
+		res[i] = (st.empty()) ? -1 : st.top();
+		st.push(arr[i]);
+	}
+
+	for(int i=0;i<n;i++) {
+		cout<<res[i]<<" ";
+	}
+}
+
 int main() {
 	vector<int> arr = {5, 15, 10, 8, 6, 12, 9, 18};
+	nextGreater(arr);
 
 	return 0;
 }
